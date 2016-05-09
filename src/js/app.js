e96827805a7446c0f0099626c4e29a24b577c0f5
Takashi Kitajima <inc@2inc.org>
2016-05-09 09:46:15 +0900

    Changed to use the sass-basis-drawer

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import BasisDrawer from './drawer.js';
+import BasisDrawer from '../../node_modules/sass-basis-drawer/src/js/drawer.js';
 import BasisFixedHeader from '../../node_modules/sass-basis-layout/src/js/fixed-header.js';
 
 new BasisDrawer('._c-drawer');
