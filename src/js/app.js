db244b4599385712bdc032a05e0088bb19975a73
Takashi Kitajima <inc@2inc.org>
2016-05-10 01:08:42 +0900

    Changed to use sass-basis-menu

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@
 
 import BasisDrawer from '../../node_modules/sass-basis-drawer/src/js/drawer.js';
 import BasisFixedHeader from '../../node_modules/sass-basis-layout/src/js/fixed-header.js';
-import BasisMenu from './menu.js';
+import BasisMenu from '../../node_modules/sass-basis-menu/src/js/menu.js';
 
 new BasisDrawer();
 new BasisFixedHeader();
