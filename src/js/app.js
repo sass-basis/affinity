b9a96d7423be3b522d5cbe4d82cf4b95df8fa7a9
Takashi Kitajima <inc@2inc.org>
2016-05-27 21:38:04 +0900

    Fixed header

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,10 @@
 
 import BasisDrawer from '../../node_modules/sass-basis-drawer/src/js/drawer.js';
 import BasisFixedHeader from '../../node_modules/sass-basis-layout/src/js/fixed-header.js';
+import BasisOverlayHeader from '../../node_modules/sass-basis-layout/src/js/overlay-header.js';
 import BasisMenu from '../../node_modules/sass-basis-menu/src/js/menu.js';
 
 new BasisDrawer();
 new BasisFixedHeader();
+new BasisOverlayHeader();
 new BasisMenu();
